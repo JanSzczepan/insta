4886debd4544e1df9f4274d7a4dd9b4410c681f5
JanSzczepan <janszczepan62@gmail.com>
2022-11-23 18:55:34 +0100

    Hide comment keyboard

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native'
-import { Pressable, View } from 'react-native'
+import { Keyboard, Pressable, TouchableWithoutFeedback, View } from 'react-native'
 import { FontAwesome5 } from '@expo/vector-icons'
 import { AntDesign } from '@expo/vector-icons'
 import Paragraph from '../Paragraph/Paragraph'
@@ -34,81 +34,86 @@ const Post = ({ post }) => {
    }
 
    return (
-      <View style={styles.container}>
-         <View style={styles.nameContainer}>
-            <View style={styles.miniImageContainer}>
-               <CustomImage variant='miniuser' />
-            </View>
-            <Paragraph variant={['black', 'textMedium', 'semiBold']}>
-               {first_name} {last_name}
-            </Paragraph>
-         </View>
-         <Pressable
-            onPress={() => {
-               navigateToPost(id, false)
-            }}
-         >
-            <View style={styles.imageContainer}>
-               <CustomImage
-                  variant='fullWidth'
-                  source='https://static01.nyt.com/images/2021/12/12/magazine/12mag-LOR-1/12mag-LOR-1-superJumbo.jpg'
-               />
-            </View>
-         </Pressable>
-         <View style={styles.contentWrapper}>
-            <View style={styles.iconsContainer}>
-               {isLikeLoading ? (
-                  <AntDesign
-                     name='heart'
-                     size={theme.SIZES.xlarge}
-                     color={theme.COLORS.red}
-                     style={styles.icon}
-                  />
-               ) : (
-                  <CustomButton
-                     handleOnPress={() => likePost(id, creator_uuid)}
-                     buttonVariant='icon'
-                     disabled={isLiked === undefined}
-                  >
-                     {isLiked ? (
-                        <AntDesign
-                           name='heart'
-                           size={theme.SIZES.xlarge}
-                           color={theme.COLORS.red}
-                        />
-                     ) : (
-                        <AntDesign
-                           name='hearto'
-                           size={theme.SIZES.xlarge}
-                           color={theme.COLORS.black}
-                        />
-                     )}
-                  </CustomButton>
-               )}
-               <CustomButton
-                  handleOnPress={() => navigateToPost(id, true)}
-                  buttonVariant='icon'
-               >
-                  <FontAwesome5
-                     name='comment'
-                     size={theme.SIZES.xlarge}
-                     color={theme.COLORS.black}
-                  />
-               </CustomButton>
-            </View>
-            <View style={styles.likesContainer}>
-               <Paragraph variant={['black', 'textMedium', 'semiBold']}>{returnLikes(likes)}</Paragraph>
-            </View>
-            <View style={styles.commentContainer}>
-               <View style={styles.commentNameContainer}>
-                  <Paragraph variant={['black', 'textMedium', 'semiBold']}>
-                     {first_name} {last_name}
-                  </Paragraph>
+      <TouchableWithoutFeedback
+         onPress={Keyboard.dismiss}
+         accessible={false}
+      >
+         <View style={styles.container}>
+            <View style={styles.nameContainer}>
+               <View style={styles.miniImageContainer}>
+                  <CustomImage variant='miniuser' />
                </View>
-               <Paragraph variant={['black', 'textSmall']}>{description}</Paragraph>
+               <Paragraph variant={['black', 'textMedium', 'semiBold']}>
+                  {first_name} {last_name}
+               </Paragraph>
+            </View>
+            <Pressable
+               onPress={() => {
+                  navigateToPost(id, false)
+               }}
+            >
+               <View style={styles.imageContainer}>
+                  <CustomImage
+                     variant='fullWidth'
+                     source='https://static01.nyt.com/images/2021/12/12/magazine/12mag-LOR-1/12mag-LOR-1-superJumbo.jpg'
+                  />
+               </View>
+            </Pressable>
+            <View style={styles.contentWrapper}>
+               <View style={styles.iconsContainer}>
+                  {isLikeLoading ? (
+                     <AntDesign
+                        name='heart'
+                        size={theme.SIZES.xlarge}
+                        color={theme.COLORS.red}
+                        style={styles.icon}
+                     />
+                  ) : (
+                     <CustomButton
+                        handleOnPress={() => likePost(id, creator_uuid)}
+                        buttonVariant='icon'
+                        disabled={isLiked === undefined}
+                     >
+                        {isLiked ? (
+                           <AntDesign
+                              name='heart'
+                              size={theme.SIZES.xlarge}
+                              color={theme.COLORS.red}
+                           />
+                        ) : (
+                           <AntDesign
+                              name='hearto'
+                              size={theme.SIZES.xlarge}
+                              color={theme.COLORS.black}
+                           />
+                        )}
+                     </CustomButton>
+                  )}
+                  <CustomButton
+                     handleOnPress={() => navigateToPost(id, true)}
+                     buttonVariant='icon'
+                  >
+                     <FontAwesome5
+                        name='comment'
+                        size={theme.SIZES.xlarge}
+                        color={theme.COLORS.black}
+                     />
+                  </CustomButton>
+               </View>
+               <View style={styles.likesContainer}>
+                  <Paragraph variant={['black', 'textMedium', 'semiBold']}>{returnLikes(likes)}</Paragraph>
+               </View>
+               <View style={styles.commentContainer}>
+                  <View style={styles.commentNameContainer}>
+                     <Paragraph variant={['black', 'textMedium', 'semiBold']}>
+                        {first_name} {last_name}
+                     </Paragraph>
+                  </View>
+                  <Paragraph variant={['black', 'textSmall']}>{description}</Paragraph>
+               </View>
             </View>
          </View>
-      </View>
+      </TouchableWithoutFeedback>
    )
 }
 
